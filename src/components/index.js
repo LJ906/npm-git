0daa3aa50d4bf4f24a2dbfc5b285e35629d1d64f
LJ906 <ni_hao906@163.com>
2019-06-22 17:13:35 +0800

    修改index.js, webpack

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Hello from './Hello.vue'
-
 import Bag1 from './Bag1.vue'
+
 const npmGit = {
     install,
     Hello,
@@ -11,7 +11,6 @@ if (typeof window !== 'undefined' && window.Vue) {
     window.Vue.use(install)
 }
 
-
 function install(Vue) {
     if (install.installed) {
         return
@@ -19,7 +18,6 @@ function install(Vue) {
 
     Vue.component('Hello', Hello)
     Vue.component('Bag1', Bag1)
-
 }
 
 export default npmGit
